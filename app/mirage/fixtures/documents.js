9ec0ad24c110ee1f08c5676d60465febbd9d4da3
Zach Garwood <zachgarwood@gmail.com>
2015-12-14 18:28:18 -0600

    Added Moment plugin for datetime formatting

--- app/mirage/fixtures/documents.js
+++ app/mirage/fixtures/documents.js
@@ -1,5 +1,5 @@
 export default [
-  {id: 1, name: 'One', versions: [1, 2]},
-  {id: 2, name: 'Two', versions: [3]},
-  {id: 3, name: 'Three', versions: [4]},
+  {id: 1, name: 'ABC', versions: [1, 2]},
+  {id: 2, name: 'DEF', versions: [3]},
+  {id: 3, name: 'GHI', versions: [4]},
 ];
