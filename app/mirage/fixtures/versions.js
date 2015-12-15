4c43988e6b7a777fc9ae10ba34bba275483faac4
Zach Garwood <zachgarwood@gmail.com>
2015-12-14 22:34:47 -0600

    Added referene to document in version model

--- app/mirage/fixtures/versions.js
+++ app/mirage/fixtures/versions.js
@@ -1,6 +1,6 @@
 export default [
-  {id: 1, timestamp: '2015-12-01', properties: {key: 'old value'}},
-  {id: 2, timestamp: '2015-12-02', properties: {key: 'new value', otherkey: 'other value'}},
-  {id: 3, timestamp: '2015-12-01', properties: {yup: 'nope'}},
-  {id: 4, timestamp: '2015-12-02', properties: {huh: 'what'}},
+  {id: 1, document_id: 1, timestamp: '2015-12-01', properties: {key: 'old value'}},
+  {id: 2, document_id: 1, timestamp: '2015-12-02', properties: {key: 'new value', otherkey: 'other value'}},
+  {id: 3, document_id: 2, timestamp: '2015-12-01', properties: {yup: 'nope'}},
+  {id: 4, document_id: 3, timestamp: '2015-12-02', properties: {huh: 'what'}},
 ];
--- app/version/model.js
+++ app/version/model.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
+  document: DS.belongsTo('document'),
   timestamp: DS.attr('date'),
   properties: DS.attr(),
   attributes: Ember.computed('properties', function() {
