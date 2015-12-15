9ee8299a555e3a5f105918563ee62f908c49b16b
Zach Garwood <zachgarwood@gmail.com>
2015-12-15 14:23:45 -0600

    Added more dummy data

--- app/mirage/fixtures/documents.js
+++ app/mirage/fixtures/documents.js
@@ -1,5 +1,11 @@
 export default [
-  {id: 1, name: 'ABC', versions: [1, 2]},
-  {id: 2, name: 'DEF', versions: [3]},
-  {id: 3, name: 'GHI', versions: [4]},
+  {id: 1, name: 'ABC12', versions: [1, 2]},
+  {id: 2, name: 'DEF34', versions: [3]},
+  {id: 3, name: 'GHI56', versions: [4]},
+  {id: 4, name: 'JKL78', versions: [5, 6, 7, 8]},
+  {id: 5, name: 'MNO90', versions: [9]},
+  {id: 6, name: 'PQR12', versions: [10]},
+  {id: 7, name: 'STU34', versions: [11]},
+  {id: 8, name: 'VWX56', versions: [12, 13, 14]},
+  {id: 9, name: 'YZ789', versions: [15]},
 ];
